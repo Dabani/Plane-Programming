96153b4e989b2a4596cb370669b89a1289b5d7b3
Dabani <robertmaku@yahoo.fr>
2022-08-23 12:33:00 +0200

    Fix the rotation direction and speed

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 10.0f; // Speed initialization
-    public float rotationSpeed;
+    public float rotationSpeed = 100.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
